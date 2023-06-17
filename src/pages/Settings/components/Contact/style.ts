a83647a89f9427377c145eb6bde6a79caa3f3ed2
abd qaddora <57998399+AbdQaddora@users.noreply.github.com>
2023-06-17 21:35:17 +0300

    fix some css issues

--- src/pages/Settings/components/Contact/style.ts
+++ src/pages/Settings/components/Contact/style.ts
@@ -4,7 +4,8 @@ const Style = styled.div`
     .grid{
         display: grid;
         grid-template-columns: 1fr 1fr;
-        gap: 8px;
+        row-gap: 12px;
+        column-gap: 8px;
 
         @media screen and (max-width:920px) {
             grid-template-columns: 1fr;
