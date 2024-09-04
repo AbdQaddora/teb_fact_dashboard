27f89cbf23fd598850fbe796aed9dd945dbfacab
abd qaddora <57998399+AbdQaddora@users.noreply.github.com>
2024-09-04 21:15:23 +0300

    Update rating component

--- src/components/tiny/Rating/index.tsx
+++ src/components/tiny/Rating/index.tsx
@@ -5,7 +5,7 @@ import { AiFillStar, AiOutlineStar } from 'react-icons/ai'
 const Rating = ({ rating }: { rating: number }) => {
     return (
         <Style>
-            {[...Array(rating || 0)].map((el, index) => <AiFillStar className="star_icon" key={index} />)}
+            {[...Array(Math.round(rating) || 0)].map((el, index) => <AiFillStar className="star_icon" key={index} />)}
             {[...Array(5 - (rating || 0))].map((el, index) => <AiOutlineStar className="star_icon" key={index} />)}
         </Style>
     )
