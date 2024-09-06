d4957e32dbd02babdec52ccfc9372df8db2ebc9a
abd qaddora <57998399+AbdQaddora@users.noreply.github.com>
2024-09-06 09:32:55 +0300

    Update index.tsx

--- src/pages/Dermatologist/components/DermatologistCard/index.tsx
+++ src/pages/Dermatologist/components/DermatologistCard/index.tsx
@@ -24,7 +24,7 @@ const DermatologistCard = () => {
             <img src={dermatologist.profile_image} alt="avatar" className="avatar" />
             <H5>{dermatologist.full_name}</H5>
             <div className='dermatologist_secondary_info'>
-                <Rating rating={dermatologist.rating} />
+                
             </div>
             <Button
                 onClick={handelActivateFlip}
